51f2c1e06a39d1f7729e626fbf14421f67798eec
joelcmk <57118300+joelcmk@users.noreply.github.com>
2020-05-07 16:33:41 -0500

    Updates

---
+++ precache-manifest.a15d1bc4a66f6ec4c3c1193848be4627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bc7e940c4539615b87ff487a9e97360",
+    "url": "/meetup/index.html"
+  },
+  {
+    "revision": "f67d07b0b1a81c506a5d",
+    "url": "/meetup/static/css/main.8d4bce68.chunk.css"
+  },
+  {
+    "revision": "20bf3a42973093139544",
+    "url": "/meetup/static/js/2.0c0cf5e7.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/meetup/static/js/2.0c0cf5e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f67d07b0b1a81c506a5d",
+    "url": "/meetup/static/js/main.73aacc26.chunk.js"
+  },
+  {
+    "revision": "e12257fd0ebca8c655b0",
+    "url": "/meetup/static/js/runtime-main.59475e37.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup/precache-manifest.95d9528606e8050df9300bff59be312f.js"
+  "/meetup/precache-manifest.a15d1bc4a66f6ec4c3c1193848be4627.js"
 );
 
 self.addEventListener('message', (event) => {
